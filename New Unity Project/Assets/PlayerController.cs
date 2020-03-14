46c4068f9a540955622c58b4a6b14efcab9e0def
ricepoutine <dehfuzz33@gmail.com>
2020-03-14 15:40:49 -0600

    added basics of game and init

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    //public Rigidbody theR3;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed); //controller.transform.position.y
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+        }
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+        moveDirection.y += (Physics.gravity.y * Time.deltaTime * gravityScale);
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+}
