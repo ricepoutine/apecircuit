dc49fff2ae608e57bee9b4a385040d868b2831e4
ricepoutine <dehfuzz33@gmail.com>
2020-03-16 16:06:50 -0600

    camera player tracking

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -19,17 +19,23 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed); //controller.transform.position.y
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed); //controller.transform.position.y
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
         if (controller.isGrounded)
         {
             moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
         }
-        if (Input.GetButton("Jump"))
-        {
-            moveDirection.y = jumpForce;
-        }
+
         moveDirection.y += (Physics.gravity.y * Time.deltaTime * gravityScale);
         controller.Move(moveDirection*Time.deltaTime);
     }
